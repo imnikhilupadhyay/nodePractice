d0ad5093a41dbb20b6309fc32fb9f20655efce23
Nikhil Upadhyay <Nikhil.upadhyay@adra.com>
2018-10-13 17:19:35 +0530

    dependencies added!!

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
-const server = http.createServer((request,response)=>{
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 
+        'Content-Type': 'text/plain' 
+    });
     response.write('Hello World');
     response.end();
 });
 
-server.listen(8080,()=>{
+server.listen(8080, () => {
     console.log('sever listening..');
+});
+
+process.on('exit',()=>{
+    console.log('Server stopped !!');
 });
